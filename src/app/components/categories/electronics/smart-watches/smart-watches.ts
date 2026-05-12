df9067289a7c19ed00fb6a8affd2eef1d82d641d
Debabrata Das <debabratadas711@gmail.com>
2026-05-12 23:17:34 +0530

    12-05-2026

---
+++ src/app/components/categories/electronics/smart-watches/smart-watches.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-watches',
+  imports: [],
+  templateUrl: './smart-watches.html',
+  styleUrl: './smart-watches.css',
+})
+export class SmartWatches {
+
+}
---
+++ src/app/components/categories/footwear/footwear.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footwear',
+  imports: [],
+  templateUrl: './footwear.html',
+  styleUrl: './footwear.css',
+})
+export class Footwear {
+
+}
