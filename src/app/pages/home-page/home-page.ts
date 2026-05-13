90c9e0e03644ef9d1a5cf68b7a2486b4382e4290
Debabrata Das <debabratadas711@gmail.com>
2026-05-13 22:03:19 +0530

    13-05-2026

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -7,11 +7,28 @@ import { Mobiles } from '../../components/categories/electronics/mobiles/mobiles
 import { Discount } from '../../components/categories/discount/discount';
 import { MoreItems } from '../../components/categories/more-items/more-items';
 import { TodayDeals } from '../../components/categories/today-deals/today-deals';
+import { SmartWatches } from '../../components/categories/electronics/smart-watches/smart-watches';
+import { Laptops } from '../../components/categories/electronics/laptops/laptops';
+import { Footwear } from '../../components/categories/footwear/footwear';
+import { Headphones } from '../../components/categories/electronics/headphones/headphones';
+import { Trending } from '../../components/categories/trending/trending';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink, Hero, Mobiles, Discount, MoreItems, TodayDeals],
+  imports: [
+    RouterLink,
+    Hero,
+    Mobiles,
+    Discount,
+    MoreItems,
+    TodayDeals,
+    SmartWatches,
+    Laptops,
+    Footwear,
+    Headphones,
+    Trending,
+  ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
