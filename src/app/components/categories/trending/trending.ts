a3ff8af47508243345c251019494e05dbefa8442
Debabrata Das <debabratadas711@gmail.com>
2026-05-13 23:00:06 +0530

    add new

--- src/app/components/categories/trending/trending.ts
+++ src/app/components/categories/trending/trending.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './trending.html',
   styleUrl: './trending.css',
 })
-export class Trending {
+export class Trending implements OnInit {
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
+  @Input() totalLimit = 24;
+
+  products = signal<Product[]>([]);
+
+  currentIndex = signal(0);
+  pageSize = 8;
+
+  fullProducts = computed(() => {
+    return this.products().slice(0, this.totalLimit);
+  });
+
+  limitedProducts = computed(() => {
+    const start = this.currentIndex();
+    const end = start + this.pageSize;
+
+    return this.fullProducts().slice(start, end);
+  });
+
+  ngOnInit(): void {
+    const productSub = this.productService.getTrendingProducts().subscribe({
+      next: (res) => {
+        this.products.set(res);
+        console.log(res);
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  next() {
+    const maxIndex = this.fullProducts().length - this.pageSize;
+
+    if (this.currentIndex() < maxIndex) {
+      this.currentIndex.set(this.currentIndex() + this.pageSize);
+    }
+  }
+
+  prev() {
+    if (this.currentIndex() > 0) {
+      this.currentIndex.set(this.currentIndex() - this.pageSize);
+    }
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
 }
