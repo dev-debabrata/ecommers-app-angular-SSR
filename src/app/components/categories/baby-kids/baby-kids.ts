609a95c8f4e8e38a62402ecb29eea2b86b6de89b
Debabrata Das <debabratadas711@gmail.com>
2026-05-15 22:45:04 +0530

    15-05-2026

--- src/app/components/categories/baby-kids/baby-kids.ts
+++ src/app/components/categories/baby-kids/baby-kids.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-baby-kids',
+  standalone: true,
   imports: [],
   templateUrl: './baby-kids.html',
   styleUrl: './baby-kids.css',
 })
-export class BabyKids {
+export class BabyKids implements OnInit {
+  private router = inject(Router);
+  private productsService = inject(ProductService);
+  private destroyRef = inject(DestroyRef);
 
+  @Input() category: string = 'baby-kids';
+
+  products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    const productSub = this.productsService.getProductsByCategory(this.category).subscribe({
+      next: (res: Product[]) => {
+        this.products.set(res.slice(0, 4));
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
 }
