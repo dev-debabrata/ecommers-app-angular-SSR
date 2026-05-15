b4293f8f7981726e2701883722d0ecb6c337f5dc
Debabrata Das <debabratadas711@gmail.com>
2026-05-15 22:41:31 +0530

    15-05-2026

--- src/app/components/categories/beauty/beauty.ts
+++ src/app/components/categories/beauty/beauty.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-beauty',
+  standalone: true,
   imports: [],
   templateUrl: './beauty.html',
   styleUrl: './beauty.css',
 })
-export class Beauty {
+export class Beauty implements OnInit {
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
+  @Input() category: string = 'beauty';
+
+  products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    const productSub = this.productService.getProductsByCategory(this.category).subscribe({
+      next: (res: Product[]) => {
+        this.products.set(res.slice(0, 4));
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
 }
