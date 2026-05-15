23987265849d8d761948bf9960ceca255fa60005
Debabrata Das <debabratadas711@gmail.com>
2026-05-15 22:43:10 +0530

    add new

--- src/app/components/categories/jewellery/jewellery.ts
+++ src/app/components/categories/jewellery/jewellery.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-jewellery',
+  standalone: true,
   imports: [],
   templateUrl: './jewellery.html',
   styleUrl: './jewellery.css',
 })
-export class Jewellery {
+export class Jewellery implements OnInit {
+  private router = inject(Router);
+  private productService = inject(ProductService);
+  private destroyRef = inject(DestroyRef);
 
+  @Input() category: string = 'jewellery';
+
+  products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    const productSub = this.productService.getProductsByCategory(this.category).subscribe({
+      next: (res: Product[]) => {
+        this.products.set(res.slice(0, 4));
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
 }
