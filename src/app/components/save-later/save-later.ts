49128faa8e224c80c42d1e5534bae92b19596977
Debabrata Das <debabratadas711@gmail.com>
2026-05-16 22:47:15 +0530

    add new

---
+++ src/app/components/save-later/save-later.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-later',
+  standalone: true,
+  imports: [],
+  templateUrl: './save-later.html',
+  styleUrl: './save-later.css',
+})
+export class SaveLater {}
