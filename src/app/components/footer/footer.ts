55b3521195325953a07f7423b406b5672232e5df
Debabrata Das <debabratadas711@gmail.com>
2026-05-20 23:01:40 +0530

    fixed all

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faFacebookF,
+  faInstagram,
+  faXTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   currentYear = new Date().getFullYear();
+
+  faFacebook = faFacebookF;
+  faInstagram = faInstagram;
+  faXTwitter = faXTwitter;
+  faYoutube = faYoutube;
 }
