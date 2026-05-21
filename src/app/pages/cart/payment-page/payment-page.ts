8b786e59a32e715b2371287878271848f131f31a
Debabrata Das <debabratadas711@gmail.com>
2026-05-21 22:25:05 +0530

    add

--- src/app/pages/cart/payment-page/payment-page.ts
+++ src/app/pages/cart/payment-page/payment-page.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-payment-page',
+  standalone: true,
   imports: [],
   templateUrl: './payment-page.html',
   styleUrl: './payment-page.css',
 })
-export class PaymentPage {
-
-}
+export class PaymentPage {}
